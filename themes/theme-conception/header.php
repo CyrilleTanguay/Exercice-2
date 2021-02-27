873b4b9f73d99a295521a59235b38d2e5bc14989
CyrilleTanguay <e1974422@cmaisonneuve.qc.ca>
2021-02-26 23:25:32 -0500

    Version finale

--- themes/theme-conception/header.php
+++ themes/theme-conception/header.php
@@ -47,7 +47,12 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'theme-conception' ); ?></button>
+			<button id="burger" aria-controls="primary-menu" aria-expanded="false">
+			<div></div>
+        <div></div>
+        <div class='ouvreX3'></div>
+			</button> 
+			
 			<?php
 			wp_nav_menu(
 				array(
